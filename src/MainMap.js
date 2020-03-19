f11cc6f99ae1af36fab1335aa0e64206a221b886
Michal Stuglik <stuglik@gmail.com>
2020-03-19 17:27:38 +0100

    Add zoom-box

--- src/MainMap.js
+++ src/MainMap.js
@@ -2,7 +2,8 @@ import React from 'react'
 import {FeatureGroup, GeoJSON, LayersControl, Map, TileLayer} from 'react-leaflet';
 import axios from 'axios';
 import {CoordinatesControl} from 'react-leaflet-coordinates';
-// import 'leaflet/dist/leaflet.css'
+import { BoxZoomControl } from 'react-leaflet-box-zoom'
+import Control from 'react-leaflet-control';
 
 const {BaseLayer, Overlay} = LayersControl;
 
@@ -96,6 +97,28 @@ export default class MainMap extends React.Component {
                     </LayersControl>
 
                     <CoordinatesControl position='bottomleft'/>
+
+                    <BoxZoomControl
+                        position="topleft"
+                        sticky={false}
+                    />
+
+                    {/*<Control position="topleft" >*/}
+                    {/*    <button*/}
+                    {/*        // onClick={ () => this.setState({bounds: [51.3, 0.7]}) }*/}
+                    {/*    >*/}
+
+                    {/*    </button>*/}
+
+                    {/*    <div>*/}
+                    {/*    <h2>What is Lorem Ipsum?</h2>*/}
+                    {/*    <p>*/}
+                    {/*    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.*/}
+
+                    {/*    </p>*/}
+                    {/*    </div>*/}
+                    {/*</Control>*/}
+
                 </Map>
             </div>
         )
