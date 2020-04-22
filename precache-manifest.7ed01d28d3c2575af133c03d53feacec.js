fe84135f238eca1a303003a7e6ccdffe2863b690
Michal Stuglik <stuglik@gmail.com>
2020-04-22 21:05:23 +0200

    Updates

---
+++ precache-manifest.7ed01d28d3c2575af133c03d53feacec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c69af8905f9701e34deb02183eb0f3a",
+    "url": "/idk-wyreby/index.html"
+  },
+  {
+    "revision": "643a754aeea950b541b3",
+    "url": "/idk-wyreby/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "735884748d242d978ff9",
+    "url": "/idk-wyreby/static/css/main.a04bf0ca.chunk.css"
+  },
+  {
+    "revision": "643a754aeea950b541b3",
+    "url": "/idk-wyreby/static/js/2.a84ecb82.chunk.js"
+  },
+  {
+    "revision": "acb29fef48adc9c5bcfac36459560713",
+    "url": "/idk-wyreby/static/js/2.a84ecb82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "735884748d242d978ff9",
+    "url": "/idk-wyreby/static/js/main.8ce1e310.chunk.js"
+  },
+  {
+    "revision": "1971d1215f2295c858ff",
+    "url": "/idk-wyreby/static/js/runtime-main.f98a2814.js"
+  }
+]);
